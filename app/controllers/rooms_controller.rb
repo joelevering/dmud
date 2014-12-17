683f20751f6d81b8da5e8e6d99dd070922ad2ad1
Joe Levering <coyoteejl@gmail.com>
2014-12-17 00:00:26 -0600

    Add New/Create routes for Rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,25 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      flash[:success] = "YATTA"
+      redirect_to @room
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :description)
+  end
 end
