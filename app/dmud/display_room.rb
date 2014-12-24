9a79c15d0de8db275c0bb12813a6feeeb61f7b41
Joe Levering <coyoteejl@gmail.com>
2014-12-24 10:15:23 -0500

    Display items in rooms

--- app/dmud/display_room.rb
+++ app/dmud/display_room.rb
@@ -10,11 +10,21 @@ class DisplayRoom
 
     #{@room.description}
 
+    Items:#{show_items}
+
     Exits:#{show_exits}
     display
   end
 
   private
+
+  def show_items
+    return if @room.items.empty?
+
+    @room.items.inject("") do |items_to_display, item|
+      items_to_display << " #{item.name}"
+    end
+  end
 
   def show_exits
     @room.exits.inject("") do |exits_to_display, room_exit|
